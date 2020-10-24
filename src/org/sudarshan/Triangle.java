46c593fcb08736dfa2e419a33ca40c04ca25ba68
sgphule <sgphule.gmail.com>
2020-10-24 16:01:58 +0200

    Bean autowiring

--- src/org/sudarshan/Triangle.java
+++ src/org/sudarshan/Triangle.java
@@ -4,19 +4,38 @@ import java.util.List;
 
 public class Triangle 
 {
-	private List<Point> points;
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
 		
-	public List<Point> getPoints() {
-		return points;
+	public Point getPointA() {
+		return pointA;
 	}
-	public void setPoints(List<Point> points) {
-		this.points = points;
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
 	}
 
 	public void draw() 
 	{
-		for(Point point:points) {
-			System.out.println("Point A = (" + point.getX() + ", " +point.getY() + ")");
-		}
+		System.out.println("Point A = (" + getPointA().getX() + ", " + getPointA().getY() + ")");
+		System.out.println("Point B = (" + getPointB().getX() + ", " + getPointB().getY() + ")");
+		System.out.println("Point C = (" + getPointC().getX() + ", " + getPointC().getY() + ")");
 	}
 }
